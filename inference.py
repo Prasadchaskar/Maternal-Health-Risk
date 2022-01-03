58928ea2a393d7530e6849f2d4014f44b69b1a19
Chaskar Prasad Bhausaheb <72083319+Prasadchaskar@users.noreply.github.com>
2022-01-03 15:34:10 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,9 +1,9 @@
 import pickle
 import numpy as np
 import pandas as pd
-model = pickle.load(open('MaternalHealth\maternal.pkl', 'rb'))
-scalar = pickle.load(open('MaternalHealth\scalar.pkl', 'rb'))
-class_names = ['High Risk','Low Risk','Mid Risk']
+model = pickle.load(open('maternal.pkl', 'rb'))
+scalar = pickle.load(open('scalar.pkl', 'rb'))
+class_names = ['Risk Level is High','Risk Level is Low','Risk Level is Mid']
 
 def predict(df):
     df = df[['Age','SystolicBP','DiastolicBP','BS','BodyTemp','HeartRate']]
@@ -12,19 +12,3 @@ def predict(df):
     output = [class_names[class_predicted] for class_predicted in predictions]
     return output
 
-Age=25						
-SystolicBP=130	
-DiastolicBP=80	
-BS=	15.0
-BodyTemp=98.0	
-HeartRate=86
-df = pd.DataFrame({ 
-    'Age':[Age],
-    'SystolicBP':[SystolicBP], 
-    'DiastolicBP':[DiastolicBP], 
-    'BS':[BS], 
-    'BodyTemp':[BodyTemp],
-    'HeartRate':[HeartRate]
-
-})
-print(predict(df))
